c53a46d444261bc0e9e6e6e8e8f9d66d27b92b46
Philippe Cutillas <pcutillas@email.arizona.edu>
2020-07-06 19:03:16 -0700

    Remade into package

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import qtmodern
+from setuptools import setup
+
+_version = qtmodern.__version__
+
+setup(name='qtmodern',
+      version=_version,
+      packages=['qtmodern'],
+      description='Qt Widgets Modern User Interface',
+      long_description=open('README.rst').read(),
+      author='Facade Technologies',
+      author_email='cto@facade-technologies.com',
+      url='https://github.com/facade-technologies-inc/qtmodern',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: User Interfaces'
+      ],
+      package_data={
+          'qtmodern': ['resources/*']
+      },
+      install_requires=['PySide2==5.15.0'])
